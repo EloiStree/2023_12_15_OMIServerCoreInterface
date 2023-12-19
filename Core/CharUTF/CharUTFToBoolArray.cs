20846b7c450cf73665e9ee216f18ccc2a01edc89
Éloi Strée <eloistree@gmail.com>
2023-12-19 06:57:15 +0100

    Save: 12/19/2023 6:57 AM

--- Core/CharUTF/CharUTFToBoolArray.cs
+++ Core/CharUTF/CharUTFToBoolArray.cs
@@ -1,5 +1,12 @@
 ﻿[System.Serializable]
 public class CharUTFToBoolArray : CharUTFToValue<bool>, I_CharUTFToValueBoolean
 {
-
+    public CharUTFToBoolArray(string c, bool[] array)
+    {
+        SetCharAndArray(c, array);
+    }
+    public CharUTFToBoolArray()
+    {
+        SetCharAndArray("", new bool[0]);
+    }
 }
--- Core/CharUTF/CharUTFToQuaternionArray.cs
+++ Core/CharUTF/CharUTFToQuaternionArray.cs
@@ -3,5 +3,12 @@
 [System.Serializable]
 public class CharUTFToQuaternionArray : CharUTFToValue<Quaternion>, I_CharUTFToValueQuaternion
 {
-
+    public CharUTFToQuaternionArray(string c, Quaternion[] array)
+    {
+        SetCharAndArray(c, array);
+    }
+    public CharUTFToQuaternionArray()
+    {
+        SetCharAndArray("", new Quaternion[0]);
+    }
 }
